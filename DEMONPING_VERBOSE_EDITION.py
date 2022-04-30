044f4997152387acf6613a868d644dbea404161f
ToboSK <82178536+ToboSK@users.noreply.github.com>
2022-04-30 10:57:17 +0200

    Add files via upload

---
+++ DEMONPING_VERBOSE_EDITION.py
@@ -0,0 +1,79 @@
+from ping3 import *
+from colorama import *
+import ctypes
+from colorama import init, Fore, Back, Style
+import colorama
+from os import system, name
+from sys import *
+import sys
+
+def clear():
+    if name == 'nt':
+        _ = system('cls')
+
+def delete_last_line():
+    sys.stdout.write('\x1b[1A')
+    sys.stdout.write('\x1b[2K')
+
+clear()
+
+ctypes.windll.kernel32.SetConsoleTitleW("DEMON PING - VERBOSE EDITION; VERSION 1.3 ; STATUS: IDLE")
+
+init(convert=True)
+init()
+colorama.init()
+
+def pinger():
+    pings = 0
+    for i in range (9999999):
+        verbose_ping(Target)
+        pings += 4
+        ctypes.windll.kernel32.SetConsoleTitleW("DEMON PING ; VERSION 1.2 ; STATUS: RUNNING ; PINGS: " + str(pings))
+
+time.sleep(0.02)
+print(Fore.LIGHTRED_EX + "   ,    ,    /\   /\          ")
+time.sleep(0.02)
+print(Fore.LIGHTRED_EX + "  /( /\ )\  _\ \_/ /_         ")    
+time.sleep(0.02)          
+print(Fore.LIGHTRED_EX + "  |\_||_/| < \_   _/ >        ")
+time.sleep(0.02)
+print(Fore.LIGHTRED_EX + "  \______/  \|0   0|/         ")
+time.sleep(0.02)
+print(Fore.LIGHTRED_EX + "    _\/_   _(_  ^  _)_        ")
+time.sleep(0.02)
+print(Fore.LIGHTRED_EX + "   ( () ) /`\|V'""V|/`\       ")
+time.sleep(0.02)
+print(Fore.LIGHTRED_EX + "     {}   \  \_____/  /       ")
+time.sleep(0.02)
+print(Fore.LIGHTRED_EX + "     ()   /\   )=(   /\       ")
+time.sleep(0.02)
+print(Fore.LIGHTRED_EX + "     {}  /  \_/\=/\_/  \      ")
+time.sleep(0.02)
+
+print(Fore.WHITE + "\n[" + Fore.LIGHTRED_EX + "DEMONPING" + Fore.WHITE + "] SELECT TARGET.")
+print(Fore.WHITE + "╔══[user" + Fore.LIGHTRED_EX + "@" + Fore.WHITE + "DEMONPING]")
+print(Fore.LIGHTRED_EX + "╚═" + Fore.WHITE + "═════" + Fore.LIGHTRED_EX + "> " , end='')
+Target = input()
+print(Fore.WHITE + "\n[" + Fore.LIGHTRED_EX + "DEMONPING" + Fore.WHITE + "] START NOW? y/n")
+print(Fore.WHITE + "╔══[user" + Fore.LIGHTRED_EX + "@" + Fore.WHITE + "DEMONPING]")
+print(Fore.LIGHTRED_EX + "╚═" + Fore.WHITE + "═════" + Fore.LIGHTRED_EX + "> " , end='')
+Start = input()
+if Start == ("y"):
+    running = Fore.WHITE + "\n[" + Fore.LIGHTRED_EX + "DEMONPING" + Fore.WHITE + "] RUNNING.\n"
+    for char in running:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(0.03)
+    titlecheck = Fore.WHITE + "[" + Fore.LIGHTRED_EX + "DEMONPING" + Fore.WHITE + "] CHECK TITLE BAR FOR INFO."
+    for char in titlecheck:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(0.03)
+    pinger()
+elif Start == ("n"):
+    print("Closing...")
+    exit()
+else:
+    print("Wrong answer. Closing...")
+    exit()
+
---
+++ README.md
@@ -0,0 +1,28 @@
+# DEMONPING 🔥
+## What does this program do?
+### Simple pinger made in python. Maybe it will make you look like hacker or something. :D
+# 
+## What does it look like?
+![](https://cdn.discordapp.com/attachments/926208049838977082/969882534278201404/unknown.png)
+![](https://cdn.discordapp.com/attachments/926208049838977082/969882534487945226/unknown.png)
+### Verbose Edition
+![](https://cdn.discordapp.com/attachments/926208049838977082/969882646987558922/unknown.png)
+#
+## Requirements:
+### 1. Windows (Does not work on linux because of Windows code in program.)
+### 2. Python 3
+### 3. PIP (Automatically installed with python.)
+#
+## How to install?
+### Normal Edition:
+### 1. py -m pip install colorama ping3
+### (If not working then use "python -m pip install colorama ping3")
+### 2. Be in the same folder as DEMONPING.py and run "py DEMONPING.py" or "python DEMONPING.py".
+#
+### Verbose Edition:
+### 1. py -m pip install colorama ping3
+### (If not working then use "python -m pip install colorama ping3")
+### 2. Be in the same folder as DEMONPING_VERBOSE_EDITION.py and run "py DEMONPING_VERBOSE_EDITION.py" or "python DEMONPING_VERBOSE_EDITION.py".
+#
+## Disclaimer
+### This program is not DDOSing. It's really slow to be powerful. :D
